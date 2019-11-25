939b46039db1548e4ba621a364e5df22da810238
Catalin Tudorache <tumf87@gmail.com>
2019-11-25 21:27:20 +0200

    Added yet another method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,4 +7,8 @@ public class Main {
     public static void doSomething() {
         System.out.println("Doing something really cool");
     }
+
+    public static void doSomethingElse() {
+        System.out.println("Doing something else");
+    }
 }
