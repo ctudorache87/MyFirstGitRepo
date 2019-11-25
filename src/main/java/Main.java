47ddf769d1143e746cad6f87be2373a3431ee8ee
Catalin Tudorache <tumf87@gmail.com>
2019-11-25 21:23:34 +0200

    Added new method. Added car class.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
+    public static void doSomething() {
+        System.out.println("Doing something really cool");
+    }
 }
